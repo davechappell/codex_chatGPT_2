8f792073ff8acc6c4a22047f3c97577dde58323c
Dave Chappell <dave.chappell@corcoran.com>
2023-01-17 15:54:49 -0500

    2nd commit

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from the server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://daves-ai-chatbot.onrender.com', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json',
